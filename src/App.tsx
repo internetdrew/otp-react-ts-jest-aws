329607ce43bc408d67c5e52e7a3ef83a8bedeac3
Andrew Rowley <andrew@internetdrew.com>
2024-08-20 11:27:49 -0400

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='h-screen flex items-center justify-center'>
+      <h1 className='text-red-500 text-2xl'>hello world</h1>
+    </div>
+  );
+}
+
+export default App;
