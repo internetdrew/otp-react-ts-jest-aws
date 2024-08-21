467cbab0e5ddd6fb65f73725804143cefe6cb625
Andrew Rowley <andrew@internetdrew.com>
2024-08-20 20:58:50 -0400

    handle input sanitization and progressive input focus

--- src/components/OneTimePasswordForm.tsx
+++ src/components/OneTimePasswordForm.tsx
@@ -2,7 +2,30 @@
 Current Work-In-Progress
 */
 
+import { useEffect, useRef } from 'react';
+
 const OneTimePasswordForm = () => {
+  const inputRefs = useRef<HTMLInputElement[]>([]);
+
+  const handleInput = (e: React.FormEvent<HTMLInputElement>, index: number) => {
+    const value = e.currentTarget.value;
+    if (isNaN(Number(value)) || value.length > 1) {
+      e.currentTarget.value = '';
+      return;
+    }
+
+    if (value.length > 1) {
+      e.currentTarget.value = value.slice(0, 1);
+    }
+    if (value.length === 1 && index < inputRefs.current.length - 1) {
+      inputRefs.current[index + 1].focus();
+    }
+  };
+
+  useEffect(() => {
+    inputRefs?.current[0].focus();
+  }, []);
+
   return (
     <form className='max-w-md bg-slate-200 flex flex-col gap-4 p-6 rounded-md'>
       <div>
@@ -17,6 +40,8 @@ const OneTimePasswordForm = () => {
               inputMode='numeric'
               maxLength={1}
               className='p-2 flex-1 w-1/6 rounded-md no-spinner text-slate-900 text-center text-xl'
+              ref={el => (inputRefs.current[index] = el!)}
+              onInput={e => handleInput(e, index)}
             />
           );
         })}
