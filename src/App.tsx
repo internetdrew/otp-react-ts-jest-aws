68d50946bee047b3c29af210a776bfa0c7848b59
Andrew Rowley <andrew@internetdrew.com>
2024-08-23 08:39:39 -0400

    add toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import OneTimePasswordForm from './components/OneTimePasswordForm';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <div className='h-screen flex items-center justify-center bg-neutral-950'>
       <OneTimePasswordForm />
+      <Toaster />
     </div>
   );
 }
